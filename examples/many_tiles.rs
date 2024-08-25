077086629f291e8e5240e5b089d308a207499e60
TheGrimsey <TheGrimsey@users.noreply.github.com>
2024-08-25 19:47:57 +0200

    Add mip maps to test mud texture

--- examples/many_tiles.rs
+++ examples/many_tiles.rs
@@ -1,6 +1,6 @@
 use std::num::{NonZeroU32, NonZeroU8};
 
-use bevy::{app::{App, Startup}, asset::AssetServer, color::Color, core::Name, diagnostic::FrameTimeDiagnosticsPlugin, math::Vec3, pbr::{DirectionalLight, DirectionalLightBundle}, prelude::{default, Commands, Res, ResMut, Transform, TransformBundle, VisibilityBundle}, DefaultPlugins};
+use bevy::{app::{App, Startup}, asset::{AssetMode, AssetPlugin, AssetServer}, color::Color, core::Name, diagnostic::FrameTimeDiagnosticsPlugin, math::Vec3, pbr::{DirectionalLight, DirectionalLightBundle}, prelude::{default, PluginGroup, Commands, Res, ResMut, Transform, TransformBundle, VisibilityBundle}, DefaultPlugins};
 use bevy_editor_pls::EditorPlugin;
 use bevy_terrain_test::{material::{GlobalTexturingRules, TerrainTexturingSettings, TexturingRule, TexturingRuleEvaluator}, terrain::TerrainCoordinate, Heights, TerrainNoiseLayer, TerrainNoiseLayers, TerrainPlugin, TerrainSettings};
 
@@ -9,7 +9,10 @@ fn main() {
     let mut app = App::new();
 
     app.add_plugins((
-        DefaultPlugins,
+        DefaultPlugins.set(AssetPlugin {
+            mode: AssetMode::Processed,
+            ..default()
+        }),
         EditorPlugin::default(),
         FrameTimeDiagnosticsPlugin
     ));
